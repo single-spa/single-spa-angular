340a99f7b1bc1008afea419fb3b10acf197cd6c1
Joel Denning <joeldenning@gmail.com>
2018-12-28 23:51:22 -0700

    Support for angular 7, including angular@router 7.

--- src/single-spa-angular.js
+++ src/single-spa-angular.js
@@ -5,6 +5,7 @@ const defaultOpts = {
   template: null,
   // optional opts
   Router: null,
+  ApplicationRef: null,
   domElementGetter: null, // only optional if you provide a domElementGetter as a custom prop
 };
 
@@ -30,6 +31,10 @@ export default function singleSpaAngular(userOpts) {
     throw new Error(`single-spa-angular must be passed opts.template string`);
   }
 
+  if (opts.Router && !opts.ApplicationRef || opts.ApplicationRef && !opts.Router) {
+    throw Error('For @angular/router to work with single-spa, you must provide both the Router and ApplicationRef opts');
+  }
+
   return {
     bootstrap: bootstrap.bind(null, opts),
     mount: mount.bind(null, opts),
@@ -38,7 +43,25 @@ export default function singleSpaAngular(userOpts) {
 }
 
 function bootstrap(opts) {
-  return Promise.resolve();
+  return Promise.resolve().then(() => {
+    opts.routingEventListener = function(evt) {
+      /* When popstate and hashchange events occur, single-spa delays them in order to
+       * check which applications should be active and perform any necessary mounting/unmounting.
+       *
+       * ZoneJS freaks out about this because it hears about the events but it wasn't inside of a
+       * Zone.current.run() block (or similar). I tried out modifying single-spa to call the event listener
+       * inside of a Zone.run() block, but that didn't seem to help. I think if we could get that working
+       * that it would be the best solution.
+       *
+       * I also tried out trying to detect with single-spa:routing-event events are the ones that actually
+       * need to trigger an application tick, since not every one of them does. But I wasn't able to find a reliable
+       * way of detecting it. So I fell back to just always causing an application tick, even though that's probably
+       * not great for performance.
+       */
+      const applicationRef = opts.bootstrappedModule.injector.get(opts.ApplicationRef)
+      applicationRef.tick()
+    }
+  })
 }
 
 function mount(opts, props) {
@@ -58,21 +81,28 @@ function mount(opts, props) {
         .angularPlatform
         .bootstrapModule(opts.mainModule)
         .then(module => {
-          return opts.bootstrappedModule = module;
+          opts.bootstrappedModule = module;
+          if (opts.ApplicationRef) {
+            window.addEventListener('single-spa:routing-event', opts.routingEventListener)
+          }
+          return module
         })
     })
 }
 
 function unmount(opts, props) {
-  return new Promise((resolve, reject) => {
+  return Promise.resolve().then(() => {
     if (opts.Router) {
+      // Workaround for https://github.com/angular/angular/issues/19079
       const routerRef = opts.bootstrappedModule.injector.get(opts.Router);
       routerRef.dispose();
     }
+    if (opts.ApplicationRef) {
+      window.removeEventListener('single-spa:routing-event', opts.routingEventListener)
+    }
     opts.bootstrappedModule.destroy();
     delete opts.bootstrappedModule;
-    resolve();
-  });
+  })
 }
 
 function getContainerEl(domElementGetter) {
