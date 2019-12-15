83906d636c614c2968232f2e1cf4d63f131309b0
Ricardo Trindade <ricardo.trindade.ext@spms.min-saude.pt>
2019-12-15 21:08:43 +0000

    fixed parcel index

--- src/browser-lib/parcel-lib/index.ts
+++ src/browser-lib/parcel-lib/index.ts
@@ -1,9 +1,7 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { ParcelComponent } from "./parcel.component";
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [ParcelComponent],
   exports: [ParcelComponent],
   entryComponents: [ParcelComponent]
