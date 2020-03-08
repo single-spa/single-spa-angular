86bd6b30aac799e24255a2939603b57948c463d1
Joel Denning <joeldenning@gmail.com>
2020-03-07 20:37:43 -0700

    Fixing typescript issues

--- src/browser-lib/parcel-lib/parcel.component.ts
+++ src/browser-lib/parcel-lib/parcel.component.ts
@@ -63,7 +63,9 @@ export class ParcelComponent implements OnInit, OnDestroy, OnChanges {
 
   ngOnChanges() {
     this.addThingToDo('update', () => {
+      // @ts-ignore https://github.com/single-spa/single-spa/pull/468
       if (this.parcel && this.parcel.update) {
+        // @ts-ignore https://github.com/single-spa/single-spa/pull/468
         return this.parcel.update(this.customProps)
       }
     })
