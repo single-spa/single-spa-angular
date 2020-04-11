a2100558f96fe449f4916a41a72e39fff2c7cde7
Artur Androsovych <arthurandrosovich@gmail.com>
2020-04-11 18:04:43 +0300

    fix: provide custom `PlatformLocation` to intercept `popstate` and wrap callback into zone (#159)
    * fix: provide custom `PlatformLocation` to intercept `popstate` and wrap callback into zone
    
    * fix: move life cycle functions into the single file
    
    * fix: set `ngZone` after bootstrapping instead of using `inject`
    
    * fix: check that `Router` is provided
    
    * fix: patch router except providing custom location
    
    * chore: fix comment
    
    * fix: revert changes
    
    * fix: update schematics

---
+++ src/browser-lib/extra-providers.ts
@@ -0,0 +1,52 @@
+import { Injectable, NgZone, StaticProvider, Inject } from '@angular/core';
+import {
+  ɵBrowserPlatformLocation,
+  PlatformLocation,
+  LocationChangeEvent,
+  DOCUMENT,
+} from '@angular/common';
+
+@Injectable()
+export class SingleSpaPlatformLocation extends ɵBrowserPlatformLocation {
+  /**
+   * We could use the `inject` function from `@angular/core` which resolves
+   * dependencies from the currently active injector, but it's a feature
+   * of Angular 8+. Should be used when we drop support for older versions.
+   */
+  private ngZone: NgZone;
+
+  onPopState(fn: (event: LocationChangeEvent) => void): void {
+    super.onPopState(event => {
+      // Wrap any event listener into zone that is specific to some application.
+      // The main issue is `back/forward` buttons of browsers, because they invoke
+      // `history.back|forward` which dispatch `popstate` event. Since `single-spa`
+      // overrides `history.replaceState` Angular's zone cannot intercept this event.
+      // Only the root zone is able to intercept all events.
+      // See https://github.com/single-spa/single-spa-angular/issues/94 for more detail
+      this.ngZone.run(() => fn(event));
+    });
+  }
+
+  setNgZone(ngZone: NgZone): void {
+    this.ngZone = ngZone;
+  }
+}
+
+/**
+ * The `PlatformLocation` class is an "injectee" of the `PathLocationStrategy`,
+ * which creates `Subject` internally for listening on `popstate` events. We want
+ * to provide this class in the most top injector that's used during bootstrapping.
+ */
+export function getSingleSpaExtraProviders(): StaticProvider[] {
+  return [
+    {
+      provide: SingleSpaPlatformLocation,
+      useClass: SingleSpaPlatformLocation,
+      deps: [[new Inject(DOCUMENT)]],
+    },
+    {
+      provide: PlatformLocation,
+      useExisting: SingleSpaPlatformLocation,
+    },
+  ];
+}
--- src/browser-lib/single-spa-angular.ts
+++ src/browser-lib/single-spa-angular.ts
@@ -1,11 +1,13 @@
-/* eslint-disable @typescript-eslint/no-use-before-define */
+import { NgZone, Type, NgModuleRef } from '@angular/core';
 import { AppProps, LifeCycles } from 'single-spa';
 
+import { SingleSpaPlatformLocation } from './extra-providers';
+
 const defaultOpts = {
-  // required opts
-  NgZone: null,
-  bootstrapFunction: null,
-  template: null,
+  // Required opts that will be set by the library consumer.
+  NgZone: null!,
+  bootstrapFunction: null!,
+  template: null!,
   // optional opts
   Router: undefined,
   domElementGetter: undefined, // only optional if you provide a domElementGetter as a custom prop
@@ -43,114 +45,130 @@ export default function singleSpaAngular(userOpts: SingleSpaAngularOpts): LifeCy
   };
 }
 
-function bootstrap(opts, props) {
-  return Promise.resolve().then(() => {
-    // In order for multiple Angular apps to work concurrently on a page, they each need a unique identifier.
-    opts.zoneIdentifier = `single-spa-angular:${props.name || props.appName}`;
+async function bootstrap(opts: BootstrappedSingleSpaAngularOpts, props: any): Promise<void> {
+  // In order for multiple Angular apps to work concurrently on a page, they each need a unique identifier.
+  opts.zoneIdentifier = `single-spa-angular:${props.name || props.appName}`;
 
-    // This is a hack, since NgZone doesn't allow you to configure the property that identifies your zone.
-    // See https://github.com/PlaceMe-SAS/single-spa-angular-cli/issues/33,
-    // https://github.com/single-spa/single-spa-angular/issues/47,
-    // https://github.com/angular/angular/blob/a14dc2d7a4821a19f20a9547053a5734798f541e/packages/core/src/zone/ng_zone.ts#L144,
-    // and https://github.com/angular/angular/blob/a14dc2d7a4821a19f20a9547053a5734798f541e/packages/core/src/zone/ng_zone.ts#L257
-    opts.NgZone.isInAngularZone = function () {
-      // @ts-ignore
-      return window.Zone.current._properties[opts.zoneIdentifier] === true;
-    };
+  // This is a hack, since NgZone doesn't allow you to configure the property that identifies your zone.
+  // See https://github.com/PlaceMe-SAS/single-spa-angular-cli/issues/33,
+  // https://github.com/single-spa/single-spa-angular/issues/47,
+  // https://github.com/angular/angular/blob/a14dc2d7a4821a19f20a9547053a5734798f541e/packages/core/src/zone/ng_zone.ts#L144,
+  // and https://github.com/angular/angular/blob/a14dc2d7a4821a19f20a9547053a5734798f541e/packages/core/src/zone/ng_zone.ts#L257
+  opts.NgZone.isInAngularZone = function () {
+    // @ts-ignore
+    return window.Zone.current._properties[opts.zoneIdentifier] === true;
+  };
 
-    opts.routingEventListener = function () {
-      opts.bootstrappedNgZone.run(() => {
-        // See https://github.com/single-spa/single-spa-angular/issues/86
-        // Zone is unaware of the single-spa navigation change and so Angular change detection doesn't work
-        // unless we tell Zone that something happened
-      });
-    };
-  });
+  opts.routingEventListener = () => {
+    opts.bootstrappedNgZone.run(() => {
+      // See https://github.com/single-spa/single-spa-angular/issues/86
+      // Zone is unaware of the single-spa navigation change and so Angular change detection doesn't work
+      // unless we tell Zone that something happened
+    });
+  };
 }
 
-function mount(opts, props) {
-  return Promise.resolve()
-    .then(() => {
-      const domElementGetter = chooseDomElementGetter(opts, props);
-      if (!domElementGetter) {
-        throw Error(
-          `cannot mount angular application '${
-            props.name || props.appName
-          }' without a domElementGetter provided either as an opt or a prop`,
-        );
-      }
+async function mount(opts: SingleSpaAngularOpts, props: any): Promise<NgModuleRef<any>> {
+  const domElementGetter = chooseDomElementGetter(opts, props);
 
-      const containerEl = getContainerEl(domElementGetter);
-      containerEl.innerHTML = opts.template;
-    })
-    .then(() => {
-      const bootstrapPromise = opts.bootstrapFunction(props);
-      if (!(bootstrapPromise instanceof Promise)) {
-        throw Error(
-          `single-spa-angular: the opts.bootstrapFunction must return a promise, but instead returned a '${typeof bootstrapPromise}' that is not a Promise`,
-        );
-      }
+  if (!domElementGetter) {
+    throw Error(
+      `cannot mount angular application '${
+        props.name || props.appName
+      }' without a domElementGetter provided either as an opt or a prop`,
+    );
+  }
 
-      return bootstrapPromise.then(module => {
-        if (!module || typeof module.destroy !== 'function') {
-          throw Error(
-            `single-spa-angular: the opts.bootstrapFunction returned a promise that did not resolve with a valid Angular module. Did you call platformBrowser().bootstrapModuleFactory() correctly?`,
-          );
-        }
-        opts.bootstrappedNgZone = module.injector.get(opts.NgZone);
-        opts.bootstrappedNgZone._inner._properties[opts.zoneIdentifier] = true;
-        window.addEventListener('single-spa:routing-event', opts.routingEventListener);
+  const containerEl = getContainerEl(domElementGetter);
+  containerEl.innerHTML = opts.template;
+  const bootstrapPromise = opts.bootstrapFunction(props);
 
-        opts.bootstrappedModule = module;
-        return module;
-      });
-    });
+  if (!(bootstrapPromise instanceof Promise)) {
+    throw Error(
+      `single-spa-angular: the opts.bootstrapFunction must return a promise, but instead returned a '${typeof bootstrapPromise}' that is not a Promise`,
+    );
+  }
+
+  const module: NgModuleRef<any> = await bootstrapPromise;
+
+  if (!module || typeof module.destroy !== 'function') {
+    throw Error(
+      `single-spa-angular: the opts.bootstrapFunction returned a promise that did not resolve with a valid Angular module. Did you call platformBrowser().bootstrapModuleFactory() correctly?`,
+    );
+  }
+
+  const singleSpaPlatformLocation: SingleSpaPlatformLocation | null = module.injector.get(
+    SingleSpaPlatformLocation,
+    null,
+  );
+
+  // The user has to provide `BrowserPlatformLocation` only if his application uses routing.
+  // So if he provided `Router` but didn't provide `BrowserPlatformLocation` then we have to inform him.
+  if (opts.Router && singleSpaPlatformLocation === null) {
+    throw new Error(`	
+      single-spa-angular: could not retrieve extra providers from the platform injector. Did you call getSingleSpaExtraProviders() when creating platform?	
+    `);
+  }
+
+  const bootstrappedOpts = opts as BootstrappedSingleSpaAngularOpts;
+  const ngZone: NgZone = module.injector.get(opts.NgZone);
+
+  singleSpaPlatformLocation!.setNgZone(ngZone);
+  bootstrappedOpts.bootstrappedNgZone = ngZone;
+  bootstrappedOpts.bootstrappedNgZone['_inner']._properties[bootstrappedOpts.zoneIdentifier] = true;
+  window.addEventListener('single-spa:routing-event', bootstrappedOpts.routingEventListener);
+  bootstrappedOpts.bootstrappedModule = module;
+
+  return module;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function unmount(opts, props) {
-  return Promise.resolve().then(() => {
-    if (opts.Router) {
-      // Workaround for https://github.com/angular/angular/issues/19079
-      const routerRef = opts.bootstrappedModule.injector.get(opts.Router);
-      routerRef.dispose();
-    }
-    window.removeEventListener('single-spa:routing-event', opts.routingEventListener);
-    if (opts.AnimationEngine) {
-      /*
-      The BrowserAnimationsModule does not clean up after itself :'(. When you unmount/destroy the main module, the
-      BrowserAnimationsModule uses an AnimationRenderer thing to remove dom elements from the page. But the AnimationRenderer
-      defers the actual work to the TransitionAnimationEngine to do this, and the TransitionAnimationEngine doesn't actually
-      remove the dom node, but just calls "markElementAsRemoved()".
+async function unmount(opts: BootstrappedSingleSpaAngularOpts, props: any): Promise<void> {
+  if (opts.Router) {
+    // Workaround for https://github.com/angular/angular/issues/19079
+    const router = opts.bootstrappedModule.injector.get(opts.Router);
+    router.dispose();
+  }
 
-      See https://github.com/angular/angular/blob/db62ccf9eb46ee89366ade586365ea027bb93eb1/packages/animations/browser/src/render/transition_animation_engine.ts#L717
+  window.removeEventListener('single-spa:routing-event', opts.routingEventListener);
 
-      What markAsRemovedDoes is put it into an array called "collectedLeaveElements", which is all the elements that should be removed
-      after the DOM has had a chance to do any animations.
+  if (opts.AnimationEngine) {
+    /*
+    The BrowserAnimationsModule does not clean up after itself :'(. When you unmount/destroy the main module, the
+    BrowserAnimationsModule uses an AnimationRenderer thing to remove dom elements from the page. But the AnimationRenderer
+    defers the actual work to the TransitionAnimationEngine to do this, and the TransitionAnimationEngine doesn't actually
+    remove the dom node, but just calls "markElementAsRemoved()".
 
-      See https://github.com/angular/angular/blob/master/packages/animations/browser/src/render/transition_animation_engine.ts#L525
+    See https://github.com/angular/angular/blob/db62ccf9eb46ee89366ade586365ea027bb93eb1/packages/animations/browser/src/render/transition_animation_engine.ts#L717
 
-      The actual dom nodes aren't removed until the TransitionAnimationEngine "flushes".
+    What markAsRemovedDoes is put it into an array called "collectedLeaveElements", which is all the elements that should be removed
+    after the DOM has had a chance to do any animations.
 
-      See https://github.com/angular/angular/blob/db62ccf9eb46ee89366ade586365ea027bb93eb1/packages/animations/browser/src/render/transition_animation_engine.ts#L851
+    See https://github.com/angular/angular/blob/master/packages/animations/browser/src/render/transition_animation_engine.ts#L525
 
-      Unfortunately, though, that "flush" will never happen, since the entire module is being destroyed and there will be no more flushes.
-      So what we do in this code is force one more flush of the animations after the module is destroyed.
+    The actual dom nodes aren't removed until the TransitionAnimationEngine "flushes".
 
-      Ideally, we would do this by getting the TransitionAnimationEngine directly and flushing it. Unfortunately, though, it's private class
-      that cannot be imported and is not provided to the dependency injector. So, instead, we get its wrapper class, AnimationEngine, and then
-      access its private variable reference to the TransitionAnimationEngine so that we can call flush.
-      */
-      const animationEngine = opts.bootstrappedModule.injector.get(opts.AnimationEngine);
-      animationEngine._transitionEngine.flush();
-    }
-    opts.bootstrappedModule.destroy();
-    delete opts.bootstrappedModule;
+    See https://github.com/angular/angular/blob/db62ccf9eb46ee89366ade586365ea027bb93eb1/packages/animations/browser/src/render/transition_animation_engine.ts#L851
 
-    if (ivyEnabled()) {
-      removeApplicationFromDOMIfIvyEnabled(opts, props);
-    }
-  });
+    Unfortunately, though, that "flush" will never happen, since the entire module is being destroyed and there will be no more flushes.
+    So what we do in this code is force one more flush of the animations after the module is destroyed.
+
+    Ideally, we would do this by getting the TransitionAnimationEngine directly and flushing it. Unfortunately, though, it's private class
+    that cannot be imported and is not provided to the dependency injector. So, instead, we get its wrapper class, AnimationEngine, and then
+    access its private variable reference to the TransitionAnimationEngine so that we can call flush.
+    */
+    const animationEngine = opts.bootstrappedModule.injector.get(opts.AnimationEngine);
+    animationEngine._transitionEngine.flush();
+  }
+
+  opts.bootstrappedModule.destroy();
+  delete opts.bootstrappedModule;
+
+  if (ivyEnabled()) {
+    // This is an issue. Issue has been created and Angular team is working on the fix:
+    // https://github.com/angular/angular/issues/36449
+    removeApplicationFromDOMIfIvyEnabled(opts, props);
+  }
 }
 
 function ivyEnabled(): boolean {
@@ -167,7 +185,7 @@ function ivyEnabled(): boolean {
   }
 }
 
-function removeApplicationFromDOMIfIvyEnabled(opts, props): void {
+function removeApplicationFromDOMIfIvyEnabled(opts: SingleSpaAngularOpts, props: any): void {
   const domElementGetter = chooseDomElementGetter(opts, props);
   const domElement = getContainerEl(domElementGetter);
   // View Engine removes all nodes automatically when calling `NgModuleRef.destroy()`,
@@ -176,8 +194,9 @@ function removeApplicationFromDOMIfIvyEnabled(opts, props): void {
   while (domElement.firstChild) domElement.removeChild(domElement.firstChild);
 }
 
-function getContainerEl(domElementGetter): never | HTMLElement {
+function getContainerEl(domElementGetter: DomElementGetter): never | HTMLElement {
   const element = domElementGetter();
+
   if (!element) {
     throw Error('domElementGetter did not return a valid dom element');
   }
@@ -185,7 +204,7 @@ function getContainerEl(domElementGetter): never | HTMLElement {
   return element;
 }
 
-function chooseDomElementGetter(opts, props): () => HTMLElement {
+function chooseDomElementGetter(opts: SingleSpaAngularOpts, props: any): DomElementGetter {
   props = props && props.customProps ? props.customProps : props;
   if (props.domElement) {
     return () => props.domElement;
@@ -198,10 +217,11 @@ function chooseDomElementGetter(opts, props): () => HTMLElement {
   }
 }
 
-function defaultDomElementGetter(name) {
+function defaultDomElementGetter(name: string): DomElementGetter {
   return function getDefaultDomElement() {
     const id = `single-spa-application:${name}`;
-    let domElement = document.getElementById(id);
+    let domElement: HTMLElement | null = document.getElementById(id);
+
     if (!domElement) {
       domElement = document.createElement('div');
       domElement.id = id;
@@ -212,12 +232,23 @@ function defaultDomElementGetter(name) {
   };
 }
 
+type DomElementGetter = () => HTMLElement;
+
 interface SingleSpaAngularOpts {
-  NgZone: any;
-  bootstrapFunction(props: AppProps): Promise<any>;
+  NgZone: typeof NgZone;
+  bootstrapFunction(props: AppProps): Promise<NgModuleRef<any>>;
   updateFunction?(props: AppProps): Promise<any>;
   template: string;
-  Router?: any;
+  Router?: Type<any>;
   domElementGetter?(): HTMLElement;
-  AnimationEngine?: any;
+  AnimationEngine?: Type<any>;
 }
+
+interface BootstrappedSingleSpaAngularOpts extends SingleSpaAngularOpts {
+  bootstrappedNgZone: NgZone;
+  bootstrappedModule: NgModuleRef<any>;
+  routingEventListener: () => void;
+  zoneIdentifier: string;
+}
+
+export { getSingleSpaExtraProviders } from './extra-providers';
