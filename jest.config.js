8965284fab9e4ea511dd0e346d1926eb59eb7845
arturovt <arthurandrosovich@gmail.com>
2020-08-18 23:24:52 +0300

    fix: switch off downleveling when running Jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        downlevelIteration: false,
+      },
+    },
+  },
 };
