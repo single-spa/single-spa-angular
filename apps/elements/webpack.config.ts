6448af2b8c6485c9f93591943db00a65e653ee3b
Artur Androsovych <arthurandrosovich@gmail.com>
2022-04-01 19:00:09 +0300

    chore: share dependencies between integration apps (#427)

--- apps/elements/webpack.config.ts
+++ apps/elements/webpack.config.ts
@@ -1,3 +1,21 @@
 import singleSpaAngularWebpack from '../../libs/single-spa-angular/webpack';
 
-export default (config: any, options: any) => singleSpaAngularWebpack(config, options);
+export default (config: any, options: any) => {
+  config = singleSpaAngularWebpack(config, options);
+  config.externals = [
+    ...config.externals,
+    'rxjs',
+    'rxjs/operators',
+    '@angular/core',
+    '@angular/common',
+    '@angular/common/http',
+    '@angular/compiler',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/elements',
+    'single-spa-angular/internals',
+    'single-spa-angular',
+    'single-spa-angular/elements',
+  ];
+  return config;
+};
