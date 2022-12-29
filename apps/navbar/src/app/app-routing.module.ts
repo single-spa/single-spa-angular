73f383eff50230fa1de4686c174b39fd6755e042
arturovt <arthurandrosovich@gmail.com>
2022-12-29 23:42:15 +0200

    feat: upgrade to Angular 15

--- apps/navbar/src/app/app-routing.module.ts
+++ apps/navbar/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { EmptyRouteComponent } from './components/empty-route/empty-route.compon
 const routes: Routes = [{ path: '**', component: EmptyRouteComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule],
   providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
