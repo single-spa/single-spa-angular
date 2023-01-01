067d9abbcdc1428866f2316abee5b0e44e0f3339
Artur Androsovych <arthurandrosovich@gmail.com>
2023-01-01 21:12:24 +0200

    feat: upgrade to Angular 15 (#465)

--- apps/chat/src/app/app-routing.module.ts
+++ apps/chat/src/app/app-routing.module.ts
@@ -11,4 +11,4 @@ const routes: Routes = [
   },
 ];
 
-export const AppRoutingModule = RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' });
+export const AppRoutingModule = RouterModule.forRoot(routes, {});
--- apps/shop/src/app/app-routing.module.ts
+++ apps/shop/src/app/app-routing.module.ts
@@ -15,4 +15,4 @@ const routes: Routes = [
   },
 ];
 
-export const AppRoutingModule = RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' });
+export const AppRoutingModule = RouterModule.forRoot(routes, {});
