76ac7954a602e9bfb6123e227eee880947445fd9
Joel Denning <joeldenning@gmail.com>
2025-04-14 19:57:44 -0600

    Remove index.html from projects, support routing option

--- schematics/ng-add/index.ts
+++ schematics/ng-add/index.ts
@@ -2,33 +2,46 @@ import {
   apply,
   applyTemplates,
   chain,
+  externalSchematic,
   mergeWith,
   move,
   Rule,
   SchematicContext,
-  template,
   Tree,
   url,
 } from "@angular-devkit/schematics";
+import { NodePackageInstallTask } from "@angular-devkit/schematics/tasks/index.js";
 import { ReplaceChange } from "@schematics/angular/utility/change";
 import { applyChangesToFile } from "@schematics/angular/utility/standalone/util";
 import { Schema } from "./schema.js";
 import { insertImport } from "@schematics/angular/utility/ast-utils";
-import ts from "typescript";
+import ts, { Expression, Identifier, VariableStatement } from "typescript";
 import { normalize } from "path";
 import { JSONFile } from "@schematics/angular/utility/json-file";
+import {
+  addPackageJsonDependency,
+  NodeDependencyType,
+} from "@schematics/angular/utility/dependencies";
 
 export function ngAdd(options: Schema): Rule {
   return chain([
     updateMain(),
-    createSingleSpaProps(),
+    copyFiles(),
     alterAngularJson(options),
+    addDependencies(),
+    removeIndexHtml(),
+    addEmptyRoute(options),
   ]);
 }
 
 function alterAngularJson(options: Schema): Rule {
   return (tree: Tree) => {
     const angularJson = new JSONFile(tree, "angular.json");
+
+    angularJson.modify(
+      ["projects", options.project, "architect", "build", "options", "index"],
+      "./node_modules/single-spa-angular/lib/schematics/ng-add/buried-config/index.html",
+    );
 
     if (options.cors) {
       // Read the current value, if it exists.
@@ -46,11 +59,25 @@ function alterAngularJson(options: Schema): Rule {
       );
     }
 
+    if (options.outputMainJs) {
+      angularJson.modify(
+        [
+          "projects",
+          options.project,
+          "architect",
+          "build",
+          "configurations",
+          "outputHashing",
+        ],
+        "none",
+      );
+    }
+
     return tree;
   };
 }
 
-function createSingleSpaProps(): Rule {
+function copyFiles(): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const templateSource = apply(url("./files"), [
       applyTemplates({}),
@@ -102,5 +129,93 @@ function updateMain(): Rule {
       singleSpaAngularImportChange,
       propsImportChange,
     ]);
+  };
+}
+
+function removeIndexHtml(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    tree.delete("src/index.html");
+    return tree;
+  };
+}
+
+function addDependencies(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    addPackageJsonDependency(tree, {
+      type: NodeDependencyType.Default,
+      name: "single-spa",
+      version: "^7.0.0-beta.3",
+    });
+
+    context.addTask(new NodePackageInstallTask());
+
+    return tree;
+  };
+}
+
+function addEmptyRoute(options: Schema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    if (options.routing) {
+      const path = "src/app/app.routes.ts";
+      const sourceFile = ts.createSourceFile(
+        path,
+        tree.readText(path),
+        ts.ScriptTarget.Latest,
+        true,
+      );
+
+      const routesStatement: VariableStatement = sourceFile.statements.find(
+        (s) =>
+          ts.isVariableStatement(s) &&
+          s.declarationList.declarations.some(
+            (d) => (d.name as Identifier)?.escapedText,
+          ),
+      ) as VariableStatement;
+      const routesInitializer: Expression =
+        routesStatement.declarationList.declarations[0].initializer!;
+
+      const newRoute = ts.factory.createObjectLiteralExpression([
+        ts.factory.createPropertyAssignment(
+          "path",
+          ts.factory.createStringLiteral("**"),
+        ),
+        ts.factory.createPropertyAssignment(
+          "component",
+          ts.factory.createIdentifier("EmptyRouteComponent"),
+        ),
+      ]);
+
+      const newRoutesExpressions: Expression[] = [];
+      routesInitializer.forEachChild((child) => {
+        newRoutesExpressions.push(child as Expression);
+      });
+      newRoutesExpressions.push(newRoute);
+
+      const newRoutes: Expression =
+        ts.factory.createArrayLiteralExpression(newRoutesExpressions);
+
+      applyChangesToFile(tree, path, [
+        insertImport(
+          sourceFile,
+          path,
+          "EmptyRouteComponent",
+          "./empty-route/empty-route.component",
+        ),
+        new ReplaceChange(
+          path,
+          routesInitializer.getStart(sourceFile),
+          routesInitializer.getText(sourceFile),
+          ts
+            .createPrinter({})
+            .printNode(ts.EmitHint.Expression, newRoutes, sourceFile),
+        ),
+      ]);
+
+      return externalSchematic("@schematics/angular", "component", {
+        name: "EmptyRoute",
+        path: "src/app",
+        style: "css",
+      });
+    }
   };
 }
