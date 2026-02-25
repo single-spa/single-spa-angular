2b7656be6dc812fb747db0424ec7f176e660201b
Artur <arthurandrosovich@gmail.com>
2026-02-25 23:16:25 +0000

    feat: support function as options.template (#568)
    Allow `template` to accept a function `(props: AppProps) => string` in
    addition to a plain string. This enables dynamic template generation
    based on single-spa props at mount time.
    
    - Update `BaseSingleSpaAngularOptions` type to accept `string | ((props: AppProps) => string)`
    - Call the function with `props` in `getContainerElementAndSetTemplate` if `template` is a function
    - Update validation error message in `singleSpaAngular` to reflect both accepted types
    - Update navbar example app to use the new function form

--- libs/single-spa-community-angular/internals/src/types.ts
+++ libs/single-spa-community-angular/internals/src/types.ts
@@ -4,7 +4,7 @@ import { ApplicationRef, NgModuleRef } from '@angular/core';
 export type DomElementGetter = (props: any) => HTMLElement;
 
 export interface BaseSingleSpaAngularOptions {
-  template: string;
+  template: string | ((props: AppProps) => string);
   domElementGetter?: DomElementGetter;
   bootstrapFunction(props: AppProps): Promise<NgModuleRef<any> | ApplicationRef>;
 }
